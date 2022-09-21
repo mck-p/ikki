0157bb74fa88163410b8f0735b21766beabcb2a8
Tim Roberts <8563579+beardedtim@users.noreply.github.com>
2022-09-21 11:11:55 -0400

    ci: Add Pull Request Workflow (#2)
    * ci: Add Pull Request Workflow
    
    * ci: Add Test Suite Basics
    
    * ci: Stop running Main on PRs

---
+++ apps/client/service.test.ts
@@ -0,0 +1,9 @@
+import test from 'ava'
+
+test('Client Test Suite', (assert) => {
+  assert.log(
+    'Test suite not actually implemented. Please go and add tests to the Client application'
+  )
+
+  assert.pass()
+})
