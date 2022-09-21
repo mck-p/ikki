b0cdfa4215e2bd7e448b3586ea13c4fab7d081ef
Tim Roberts <timiroberts@gmail.com>
2022-09-21 11:09:26 -0400

    ci: Add Test Suite Basics

---
+++ apps/api/service.test.ts
@@ -0,0 +1,9 @@
+import test from 'ava'
+
+test('API Test Suite', (assert) => {
+  assert.log(
+    'Test suite not actually implemented. Please go and add tests to the API application'
+  )
+
+  assert.pass()
+})
